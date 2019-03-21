b3eeeda330a56f9d719afe8740e231add58b6788
Nicholas Rodriguez <niko876@hotmail.com>
2019-03-21 01:28:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,10 @@
 def my_collect(array)
   counter = 0
   new_array = []
-  while counter < array.length
-    yield(array[counter])
-    counter += 1
-  end
+  if array.first.upcase?
+    while counter < array.length
+      yield(array[counter])
+      counter += 1
+    end
   new_array
 end
